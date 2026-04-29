cfad82247e344f9d2adac42963473c038296617d
sc <sc@example.com>
2026-04-29 13:59:29 +0530

    for docker

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="app-root">
       <h1>React + Docker</h1>
-      <p>This app is built with Vite and served from an Nginx Docker container.</p>
+      <p>This app is built with Vite and served from an Nginx Docker container. hii</p>
     </div>
   )
 }
